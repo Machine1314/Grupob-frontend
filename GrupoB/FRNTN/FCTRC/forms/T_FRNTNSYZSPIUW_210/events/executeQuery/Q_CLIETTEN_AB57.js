719ca7633782c36e4aab3ed79ab9928f089d1902
Douglas Gallardo <douglasgallardoz11@gmail.com>
2021-04-23 10:19:51 -0500

    Se crea Formulario Cliente

---
+++ GrupoB/FRNTN/FCTRC/forms/T_FRNTNSYZSPIUW_210/events/executeQuery/Q_CLIETTEN_AB57.js
@@ -0,0 +1,6 @@
+
+//ClienteQuery Entity: Cliente
+task.executeQuery.Q_CLIETTEN_AB57 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.Cliente = true;
+};
