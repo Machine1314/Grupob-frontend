54e4d6dad73568c07dee1cec8efea39ef6773ad6
Olmer Fernando Velasquez <fernandopsmart@gmail.com>
2021-04-23 17:29:14 -0500

    pantalla producto faltante codigo en java

---
+++ GrupoB/FRNTN/FCTRC/forms/T_FRNTNBPIRNPGY_597/events/executeCommand/VA_VABUTTONBQKNNUN_370778.js
@@ -0,0 +1,11 @@
+
+
+//Entity: Producto
+//Producto. (Button) View: Insertar Producto
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONBQKNNUN_370778 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.Producto = true;
+
+};
---
+++ GrupoB/FRNTN/FCTRC/forms/T_FRNTNKOCECMZP_970/events/executeQuery/Q_PRODCTTO_OE27.js
@@ -0,0 +1,6 @@
+
+//ProductoQuery Entity: Producto
+task.executeQuery.Q_PRODCTTO_OE27 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.Producto = true;
+};
